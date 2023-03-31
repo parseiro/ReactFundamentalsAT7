23da13143cd8ad00caa671749b17cb20fef1fd41
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-31 08:33:51 -0300

    AT7

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 import './App.css'
 import {Footer} from 'flowbite-react'
-import React, {useState} from 'react'
-import States, {Estado} from './States'
-import Cities from './Cities'
+import React from 'react'
+import People from './People'
 
 export default function App() {
-    const [selectedState, setSelectedState] = useState<Estado>({id: "", nome: "", sigla: ""})
-
     return (
         <>
             <main className="flex flex-col items-center gap-4 container">
-                <h1 className="text-center text-4xl">Infinite Scroll</h1>
-                <States
-                    selectedState={selectedState}
-                    setSelectedState={setSelectedState}
-                />
+                <h1 className="text-center text-4xl">People</h1>
 
-                {selectedState && selectedState.id &&
-                  <Cities stateId={selectedState.id}/>
-                }
+                <People/>
 
             </main>
-            {/*<Footer container={true}>
+            <Footer container={true}>
                 <Footer.Copyright
                     href="https://parseiro.github.io"
                     by="Leonardo Vilela Pinheiro"
                     year={2023}
                 />
-            </Footer>*/}
+            </Footer>
         </>
     )
 }
